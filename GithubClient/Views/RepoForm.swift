bdd7cc76251becfc2319a11ee8e722d5bd74e7e7
Rod Muñoz <rodmunoz28@gmail.com>
2026-07-10 16:37:49 -0500

    Avance Laboratorio 11: Interface inicial

---
+++ GithubClient/Views/RepoForm.swift
@@ -0,0 +1,23 @@
+//
+//  RepoForm.swift
+//  GithubClient
+//
+//  Created by Rod Muñoz on 10/7/26.
+//
+
+import SwiftUI
+
+struct RepoForm: View {
+    var body: some View {
+        NavigationStack{
+            VStack{
+                Text("Formulario de Repositorios")
+            }
+            .navigationTitle("Formulario")
+        }
+    }
+}
+
+#Preview {
+    RepoForm()
+}
